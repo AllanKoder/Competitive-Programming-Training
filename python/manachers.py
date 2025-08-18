683941545cb03230b6686e5558ce7d96493469ef
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2025-08-18 15:39:10 -0700

    Manachers for real

--- python/manachers.py
+++ python/manachers.py
@@ -4,8 +4,8 @@ class Manachers():
     def __init__(self, s: str):
         self.s = s
         
-        self.new_string = '^' + str("#".join(s)) + '$'
-        self.length = [1]* len(self.new_string)
+        self.new_string = '^#' + "#".join(s) + '#$'
+        self.length = [0]* len(self.new_string)
         
         l,r = 1,1
         for i in range(1, len(self.new_string)-1):
@@ -13,22 +13,23 @@ class Manachers():
             if i < r:
                 self.length[i] = min(r-i, self.length[mirrored_index])
             
-            while self.new_string[i - self.length[i]] == self.new_string[i + self.length[i]]:
+            while self.new_string[i - 1 - self.length[i]] == self.new_string[i + 1 + self.length[i]]:
                 self.length[i] += 1
             
             if self.length[i] + i > r:
-                r = self.length[i] + i
-                l = self.length[i] - i
+                r = i + self.length[i]
+                l = i - self.length[i]
 
     def get_longest_palindrome(self):
         return max(self.length)
 
-    def get_length(self, index, odd_lengthed):
-        transformed_index = 1 + 2 * index + 0 if odd_lengthed else 1
-        return self.length(transformed_index)
-
-
+    def get_length(self, index, odd_lengthed = True):
+        transformed_index = 2 + 2 * index + (0 if odd_lengthed else 1)
+        return self.length[transformed_index]
 
 
 m = Manachers(s)
 print(m.get_longest_palindrome())
+print(m.length)
+print(m.new_string)
+print(m.get_length(2))
