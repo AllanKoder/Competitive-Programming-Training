22b325df918dc4e31bbad22cc2a84b35ee224530
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2025-09-18 18:16:09 -0600

    E

--- kattis-pratice/acpc/2023/e.py
+++ kattis-pratice/acpc/2023/e.py
@@ -1,38 +1,31 @@
 import math
 
-def raster_square_area(W, H):
-    # sum_{i=1}^H floor(i*W/H)
-    S = (W-1)*(H-1)//2 + math.gcd(W, H) - 1 + W//H * H
-    return 4 * S
-
-print(raster_square_area(12, 4))
-
-def floor_sum(n: int, m: int, a: int, b: int) -> int:
-    """
-        Compute sum_{i=0}^{n-1} floor((a*i + b) / m).
-        n: The number of terms to sum over (i = 0 to n-1).
-        m: The divisor in the floor division. Must be positive.
-        a: The coefficient multiplying i in the numerator.
-        b: The constant term added in the numerator.
-    """
-    ans = 0
-    while True:
-        if a >= m:
-            q = a // m
-            ans += (n - 1) * n * q // 2
-            a %= m
-        if b >= m:
-            ans += n * (b // m)
-            b %= m
-        y_max = a * n + b
-        if y_max < m:
-            break
-        # reduce parameters (Euclidean-style)
-        n, m, a, b = y_max // m, a, m, y_max % m
-    return ans
+# area formula
+k, w = map(int, input().split())
+rise = math.floor(w*2/k)
+length = rise + w
 
 
-# Example:
-W = 12
-K = 5
-print(floor_sum(W + 1, K, 2, 0))
+def triangle_area(w, k, rise):
+    remainder = w % k
+    smaller_rise = rise-1
+
+    odds_sum = 0
+    evens_sum = 0
+
+    if rise > 1:
+        odds = smaller_rise-1 if (smaller_rise-1)%2 == 1 else smaller_rise
+        evens = smaller_rise-1 if (smaller_rise-1)%2 == 0 else smaller_rise
+
+        odds_sum = ((odds+1)*(odds//2 + 1))//2 * math.floor(k/2)
+        evens_sum = ((evens)*(evens//2 + 1))//2 * math.ceil(k/2)
+    
+    width_of_top = remainder - math.ceil(k/2) if rise % 2 == 1 else remainder
+    remainder_sum = rise * width_of_top
+
+    return remainder_sum + odds_sum + evens_sum
+
+t_area = triangle_area(w,k,rise)
+
+area = (length**2 - t_area*4) % (10**9+9)
+print(area)
